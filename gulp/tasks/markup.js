1a9c0871f2413b9a97a55e38596ca0573ea2e5aa
Nicolas Cusan <nc@arillo.net>
2015-09-06 13:47:41 +0200

    include changed plugin in markup task to only move changed files

--- gulp/tasks/markup.js
+++ gulp/tasks/markup.js
@@ -1,11 +1,13 @@
 'use strict';
 
 var gulp = require('gulp');
+var changed = require('gulp-changed');
 var config = require('../config').markup;
 var browserSync  = require('browser-sync');
 
 gulp.task('markup', function() {
   return gulp.src(config.src)
+    .pipe(changed(config.dest))
     .pipe(gulp.dest(config.dest))
     .pipe(browserSync.reload({stream:true}));
 });
